edaf10d968970a211cc3feaa658a90f94f692a34
NNYKER <124649467+NNYKER@users.noreply.github.com>
2023-05-31 22:01:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+// Configura tu clave de API de OpenAI
+const apiKey = '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';
+
+// Configura el contenedor del chat y el área de entrada
+const chatLog = document.getElementById('chat-log');
+const userInput = document.getElementById('user-input');
+
+// Función para agregar mensajes al chat
+function appendMessage(message, sender) {
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('message', sender);
+  messageElement.innerText = message;
+  chatLog.appendChild(messageElement);
+}
+
+// Función para enviar la solicitud a la API de OpenAI y recibir la respuesta
+async function sendMessage(message) {
+  appendMessage(message, 'user');
+
+  try {
+    const response = await axios.post(
+      'https://api.openai.com/v1/engines/davinci-codex/completions',
+      {
+        prompt: message,
+        max_tokens: 50
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`
+        }
+      }
+    );
+
+    const reply = response.data.choices[0].text.trim();
+    appendMessage(reply, 'assistant');
+  } catch (error) {
+    console.error(error);
+    appendMessage('Ha ocurrido un error.', 'assistant');
+  }
+}
+
+// Evento para manejar la entrada del usuario al presionar Enter
+userInput.addEventListener('keydown', event => {
+  if (event.key === 'Enter' && userInput.value !== '') {
+    const message = userInput.value;
+    userInput.value = '';
+
+    sendMessage(message);
+  }
+});
+
+// Enfoca automáticamente el área de entrada al cargar la página
+userInput.focus();
