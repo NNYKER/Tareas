0dadaf80b875ffab7580307347717894f4bb8ea7
NNYKER <124649467+NNYKER@users.noreply.github.com>
2023-06-01 19:32:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+document.addEventListener("DOMContentLoaded", function() {
+  var chatLog = document.getElementById("chat-log");
+  var userInput = document.getElementById("user-input");
 
-import java.io.IOException;
+  // Configura tu clave de API
+  var apiKey = "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";
 
-public class OpenAIChatExample {
-    public static void main(String[] args) {
-        OkHttpClient client = new OkHttpClient();
-        MediaType mediaType = MediaType.parse("application/json");
+  // Maneja el evento de presionar Enter
+  userInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
 
-        String apiKey = "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"; // Reemplaza con tu clave de API
+      var userMessage = userInput.value;
+      addMessage("user", userMessage);
+      userInput.value = "";
 
-        String prompt = "Who won the World Series in 2020?";
-
-        String json = String.format("{\"prompt\": \"%s\", \"max_tokens\": 50, \"temperature\": 0.7}", prompt);
-
-        RequestBody requestBody = RequestBody.create(json, mediaType);
-        Request request = new Request.Builder()
-                .url("https://api.openai.com/v1/engines/davinci-codex/completions")
-                .post(requestBody)
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Bearer " + apiKey)
-                .build();
-
-        try {
-            Response response = client.newCall(request).execute();
-            if (response.isSuccessful()) {
-                String responseBody = response.body().string();
-                System.out.println(responseBody);
-                // Procesa la respuesta como desees
-            } else {
-                System.out.println("Error: " + response.code() + " " + response.message());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+      // Envía la solicitud a la API
+      axios.post("https://api.openai.com/v1/engines/davinci-codex/completions", {
+        prompt: userMessage,
+        max_tokens: 50,
+        temperature: 0.7
+      }, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + apiKey
         }
+      })
+      .then(function(response) {
+        var aiMessage = response.data.choices[0].text.trim();
+        addMessage("ai", aiMessage);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
     }
-}
+  });
+
+  // Función para agregar un mensaje al registro de chat
+  function addMessage(sender, message) {
+    var messageElement = document.createElement("div");
+    messageElement.className = "message " + sender;
+    messageElement.textContent = message;
+    chatLog.appendChild(messageElement);
+    chatLog.scrollTop = chatLog.scrollHeight;
+  }
+});
