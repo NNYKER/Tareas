9ab213562faf295518eafb7394e3cdbf539ec120
NNYKER <124649467+NNYKER@users.noreply.github.com>
2023-06-01 19:38:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var chatLog = document.getElementById("chat-log");
-  var userInput = document.getElementById("user-input");
+// Configuración de la clave de API
+const apiKey = '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';
 
-  // Configura tu clave de API
-  var apiKey = "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";
+// Función para enviar una solicitud a la API de OpenAI
+async function sendMessage(message) {
+  try {
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: message,
+      max_tokens: 50,
+      temperature: 0.7
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      }
+    });
 
-  // Maneja el evento de presionar Enter
-  userInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-
-      var userMessage = userInput.value;
-      addMessage("user", userMessage);
-      userInput.value = "";
-
-      // Envía la solicitud a la API
-      axios.post("https://api.openai.com/v1/engines/davinci-codex/completions", {
-        prompt: userMessage,
-        max_tokens: 50,
-        temperature: 0.7
-      }, {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer " + apiKey
-        }
-      })
-      .then(function(response) {
-        var aiMessage = response.data.choices[0].text.trim();
-        addMessage("ai", aiMessage);
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
-    }
-  });
-
-  // Función para agregar un mensaje al registro de chat
-  function addMessage(sender, message) {
-    var messageElement = document.createElement("div");
-    messageElement.className = "message " + sender;
-    messageElement.textContent = message;
-    chatLog.appendChild(messageElement);
+    const { choices } = response.data;
+    const chatLog = document.getElementById('chat-log');
+    const botResponse = choices[0].text.trim();
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('message', 'bot-message');
+    messageContainer.textContent = botResponse;
+    chatLog.appendChild(messageContainer);
     chatLog.scrollTop = chatLog.scrollHeight;
+  } catch (error) {
+    console.error('Error al enviar el mensaje:', error);
   }
-});
+}
+
+// Función para manejar el evento de pulsar Enter en el campo de entrada del usuario
+function handleUserInput(event) {
+  if (event.key === 'Enter') {
+    const userInput = event.target.value;
+    const chatLog = document.getElementById('chat-log');
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('message', 'user-message');
+    messageContainer.textContent = userInput;
+    chatLog.appendChild(messageContainer);
+    chatLog.scrollTop = chatLog.scrollHeight;
+    event.target.value = '';
+
+    sendMessage(userInput);
+  }
+}
+
+// Agregar un controlador de eventos al campo de entrada del usuario
+document.getElementById('user-input').addEventListener('keydown', handleUserInput);
